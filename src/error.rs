27946d3181d4540ca8c88a521d6d71f05df6710f
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-07-25 19:00:35 +0200

    + basic error struct

---
+++ src/error.rs
@@ -0,0 +1,30 @@
+use std::fmt;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub enum ErrorKind {
+}
+
+#[derive(Debug, Clone)]
+pub struct Error {
+    pub kind: ErrorKind,
+    pub message: String,
+}
+
+impl Error {
+    pub fn new(kind: ErrorKind, message: impl Into<String>) -> Error {
+        Error {
+            kind,
+            message: message.into(),
+        }
+    }
+
+    pub fn kind(&self) -> ErrorKind {
+        self.kind
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
