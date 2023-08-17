312f48a9bb1f17531e8d7900dcf8eb22270d67e1
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-08-17 17:11:38 +0200

    + token

--- src/api/input/request_header.rs
+++ src/api/input/request_header.rs
@@ -3,11 +3,12 @@ use serde::Deserialize;
 #[derive(Deserialize)]
 pub struct RequestHeader {
     action: String,
+    token: String,
 }
 
 impl RequestHeader {
-    pub fn new(action: String) -> RequestHeader {
-        RequestHeader { action }
+    pub fn new(action: String, token: String) -> RequestHeader {
+        RequestHeader { action, token }
     }
 
     pub fn action(&self) -> &str {
