fe29aa35ae21b2f417cfeb4ded4489e8cf6ae61a
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-09-22 21:44:28 +0200

    f incorrect relative path

--- run_tests.sh
+++ run_tests.sh
@@ -20,11 +20,11 @@ result_exit_code=0
 DEFAULT_AMQP_CONNECTION_URI="amqp://guest:guest@127.0.0.1:5672"
 TEST_AMQP_CONNECTION_URI=${TEST_AMQP_CONNECTION_URI:=$DEFAULT_AMQP_CONNECTION_URI}
 
-./target/debug/test_amqp_api_impl_server $TEST_AMQP_CONNECTION_URI &
+./test_amqp_api_impl_server $TEST_AMQP_CONNECTION_URI &
 impl_pid=$!
 
 sleep 1
-./target/debug/test_amqp_api_impl_client $TEST_AMQP_CONNECTION_URI
+./test_amqp_api_impl_client $TEST_AMQP_CONNECTION_URI
 
 test_amqp_api_impl_code=$?
 if [ $test_amqp_api_impl_code -eq 0 ]; then
