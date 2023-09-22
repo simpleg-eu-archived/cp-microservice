170e860c256a10ed8a3aef89ddecab7df0b6c3de
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-09-22 21:41:21 +0200

    - outdated integration tests

--- run_tests.sh
+++ run_tests.sh
@@ -15,43 +15,6 @@ cd ./target/debug
 
 result_exit_code=0
 
-# INTEGRATION TESTS
-
-# TEST EXIT, EXPECTED EXIT CODE: 1
-./test_exit
-
-test_exit_code=$?
-if [ $test_exit_code -eq 1 ]; then
-  echo "Test exit: SUCCESS"
-else
-  echo "Test exit: FAILED"
-  result_exit_code=1
-fi
-
-# TEST RUN, EXPECTED EXIT CODE: 0
-./test_run
-
-test_run_code=$?
-if [ $test_run_code -eq 0 ]; then
-  echo "Test run: SUCCESS"
-else
-  echo "Test run: FAILED"
-  result_exit_code=1
-fi
-
-# TEST WAIT FOR TASKS TO END, EXPECTED EXIT CODE: 1
-./test_wait_for_tasks_to_end
-
-test_wait_for_tasks_to_end_code=$?
-if [ $test_wait_for_tasks_to_end_code -eq 1 ]; then
-  echo "Test wait for tasks to end: SUCCESS"
-else
-  echo "Test wait for tasks to end: FAILED"
-  result_exit_code=1
-fi
-
-cd ../../
-
 # TEST AMQP API IMPL, EXPECTED EXIT CODE: 0
 
 DEFAULT_AMQP_CONNECTION_URI="amqp://guest:guest@127.0.0.1:5672"
