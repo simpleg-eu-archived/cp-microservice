1fb198862525e0d92aed6aa5b1e86ad8ee6e3690
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-10-11 16:39:22 +0200

    + avoid passing file names

--- src/impl/init.rs
+++ src/impl/init.rs
@@ -1,7 +1,7 @@
 use std::mem::Discriminant;
 use std::{collections::HashMap, sync::Arc};
 
-use async_channel::{Receiver, Sender};
+use async_channel::Sender;
 use futures_util::future::join_all;
 use multiple_connections_lapin_wrapper::{
     amqp_wrapper::AmqpWrapper, config::amqp_connect_config::AmqpConnectConfig,
@@ -13,14 +13,12 @@ use crate::r#impl::api::shared::amqp_api_entry::AmqpApiEntry;
 use crate::r#impl::process_signals::listen_to_process_signals;
 use crate::{
     api::server::input::input_plugin::InputPlugin,
-    r#impl::api::{
-        server::input::amqp_input::AmqpInput, shared::amqp_queue_consumer::AmqpQueueConsumer,
-    },
+    r#impl::api::server::input::amqp_input::AmqpInput,
 };
 
 pub struct ApiInitializationPackage<LogicRequestType: 'static + Send + Sync + std::fmt::Debug> {
-    pub amqp_connection_file: String,
-    pub amqp_api_file: String,
+    pub amqp_connection_config: AmqpConnectConfig,
+    pub amqp_api: Vec<AmqpApiEntry>,
     pub actions: HashMap<String, Action<LogicRequestType>>,
     pub plugins: Vec<Arc<dyn InputPlugin + Send + Sync>>,
 }
@@ -47,10 +45,8 @@ pub async fn try_initialize_microservice<
 
     listen_to_process_signals(cancellation_token.clone());
 
-    let amqp_connect_config: AmqpConnectConfig =
-        get_amqp_connect_config(api_initialization_package.amqp_connection_file)?;
-
-    let amqp_wrapper = match AmqpWrapper::try_new(amqp_connect_config) {
+    let amqp_wrapper = match AmqpWrapper::try_new(api_initialization_package.amqp_connection_config)
+    {
         Ok(amqp_wrapper) => amqp_wrapper,
         Err(error) => {
             return Err(std::io::Error::new(
@@ -60,9 +56,8 @@ pub async fn try_initialize_microservice<
         }
     };
 
-    let amqp_api = get_amqp_api(api_initialization_package.amqp_api_file)?;
-
-    let amqp_inputs = generate_inputs_from_api(amqp_wrapper, amqp_api).await?;
+    let amqp_inputs =
+        generate_inputs_from_api(amqp_wrapper, api_initialization_package.amqp_api).await?;
 
     let (logic_request_sender, logic_request_receiver) =
         async_channel::bounded::<LogicRequestType>(1024usize);
@@ -96,63 +91,6 @@ pub async fn try_initialize_microservice<
     tokio::spawn(logic_dispatch.run());
 
     Ok(())
-}
-
-fn get_amqp_connect_config(
-    amqp_connection_file: String,
-) -> Result<AmqpConnectConfig, std::io::Error> {
-    let amqp_connection_file_content = match std::fs::read_to_string(&amqp_connection_file) {
-        Ok(content) => content,
-        Err(error) => {
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::NotFound,
-                format!(
-                    "failed to find amqp connection file '{}': {}",
-                    &amqp_connection_file, &error
-                ),
-            ))
-        }
-    };
-
-    let amqp_connect_config =
-        match serde_json::from_str::<AmqpConnectConfig>(&amqp_connection_file_content) {
-            Ok(amqp_connect_config) => amqp_connect_config,
-            Err(error) => {
-                return Err(std::io::Error::new(
-                    std::io::ErrorKind::InvalidData,
-                    format!("failed to deserialize AMQP connection file: {}", &error),
-                ))
-            }
-        };
-
-    Ok(amqp_connect_config)
-}
-
-fn get_amqp_api<'a>(amqp_api_file: String) -> Result<Vec<AmqpApiEntry>, std::io::Error> {
-    let amqp_api_file_content = match std::fs::read_to_string(&amqp_api_file) {
-        Ok(content) => content,
-        Err(error) => {
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::NotFound,
-                format!(
-                    "failed to find amqp api file '{}': {}",
-                    &amqp_api_file, &error
-                ),
-            ))
-        }
-    };
-
-    let amqp_api = match serde_json::from_str::<Vec<AmqpApiEntry>>(&amqp_api_file_content) {
-        Ok(amqp_api) => amqp_api,
-        Err(error) => {
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::InvalidData,
-                format!("failed to deserialize AMQP API file: {}", &error),
-            ))
-        }
-    };
-
-    Ok(amqp_api)
 }
 
 async fn generate_inputs_from_api(
