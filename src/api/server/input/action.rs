4d5ce2f9c3bf5cc75b69b85d898f2b655a076773
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-10-21 13:56:54 +0200

    + common functionalities for each action

--- src/api/server/input/action.rs
+++ src/api/server/input/action.rs
@@ -1,4 +1,12 @@
-use crate::api::server::input::executor::Executor;
+use serde::{de::DeserializeOwned, Deserialize};
+
+use crate::{
+    api::{
+        server::input::{executor::Executor, plugins::token_manager::authenticator::authenticator},
+        shared::request::Request,
+    },
+    core::error::{Error, ErrorKind},
+};
 
 pub struct Action<LogicRequestType> {
     id: String,
@@ -30,4 +38,38 @@ impl<LogicRequestType> Action<LogicRequestType> {
     pub fn filter_out_plugins(&self) -> Vec<String> {
         self.filter_out_plugins.clone()
     }
+}
+
+pub fn extract_payload<PayloadType: DeserializeOwned>(
+    request: &Request,
+) -> Result<PayloadType, Error> {
+    let payload: PayloadType =
+        match serde_json::from_value::<PayloadType>(request.payload().clone()) {
+            Ok(payload) => payload,
+            Err(error) => {
+                return Err(Error::new(
+                    ErrorKind::RequestError,
+                    format!("invalid payload: {}", &error),
+                ));
+            }
+        };
+
+    Ok(payload)
+}
+
+pub fn extract_user_id(request: &Request) -> Result<String, Error> {
+    let user_id = match request
+        .header()
+        .get_extra(&authenticator::USER_ID_KEY.to_string())
+    {
+        Some(user_id) => user_id.clone(),
+        None => {
+            return Err(Error::new(
+                ErrorKind::RequestError,
+                "missing 'user_id' extra from request header",
+            ))
+        }
+    };
+
+    Ok(user_id)
 }
