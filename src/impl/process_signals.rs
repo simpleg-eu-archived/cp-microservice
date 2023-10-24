2fac91641056f9f07efdac8c07ace039746e6780
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-10-25 01:16:15 +0200

    f typo

--- src/impl/process_signals.rs
+++ src/impl/process_signals.rs
@@ -6,21 +6,21 @@ pub fn listen_to_process_signals(cancellation_token: CancellationToken) {
         let mut sigint = match tokio::signal::unix::signal(SignalKind::interrupt()) {
             Ok(sigint) => sigint,
             Err(error) => {
-                panic!("failed to lsiten to SIGINT: {}", error);
+                panic!("failed to listen to SIGINT: {}", error);
             }
         };
 
         let mut sigterm = match tokio::signal::unix::signal(SignalKind::terminate()) {
             Ok(sigterm) => sigterm,
             Err(error) => {
-                panic!("failed to lsiten to SIGTERM: {}", error);
+                panic!("failed to listen to SIGTERM: {}", error);
             }
         };
 
         let mut sigquit = match tokio::signal::unix::signal(SignalKind::quit()) {
             Ok(sigquit) => sigquit,
             Err(error) => {
-                panic!("failed to lsiten to SIGQUIT: {}", error);
+                panic!("failed to listen to SIGQUIT: {}", error);
             }
         };
 
