e10a980d7cd2817b204f0042575d338cf1cf73fb
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-10-25 01:09:57 +0200

    + expand 'Error' with easy conversion from other 'Error' types

--- src/core/error.rs
+++ src/core/error.rs
@@ -1,6 +1,8 @@
 use std::fmt;
 
+use async_channel::{RecvError, SendError};
 use serde::{Deserialize, Serialize};
+use tokio::time::error::Elapsed;
 
 #[derive(Debug, Copy, Clone, PartialEq, Serialize, Deserialize)]
 pub enum ErrorKind {
@@ -10,6 +12,7 @@ pub enum ErrorKind {
     RequestError,
     Unknown,
     InitializationError,
+    InternalError,
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -34,5 +37,35 @@ impl Error {
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", self.message)
+    }
+}
+
+impl From<Elapsed> for Error {
+    fn from(value: Elapsed) -> Self {
+        Self::new(ErrorKind::InternalError, format!("timed out: {}", &value))
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(value: std::io::Error) -> Self {
+        Self::new(ErrorKind::InternalError, format!("{}", &value))
+    }
+}
+
+impl<T> From<SendError<T>> for Error {
+    fn from(value: SendError<T>) -> Self {
+        Self::new(
+            ErrorKind::InternalError,
+            format!("failed to send request: {}", &value),
+        )
+    }
+}
+
+impl From<RecvError> for Error {
+    fn from(value: RecvError) -> Self {
+        Self::new(
+            ErrorKind::InternalError,
+            format!("failed to receive request: {}", &value),
+        )
     }
 }
