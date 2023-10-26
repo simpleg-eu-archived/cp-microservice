11f655db9009750496310f2e7df23763c1857063
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-10-26 13:40:02 +0200

    + cancellation token

--- src/storage/dispatch.rs
+++ src/storage/dispatch.rs
@@ -6,27 +6,36 @@ use std::{
 
 use async_channel::Receiver;
 use log::info;
+use tokio_util::sync::CancellationToken;
 
 use crate::storage::executor::Executor;
 
 pub struct Dispatch<StorageRequestType: Debug> {
     storage_request_receiver: Receiver<StorageRequestType>,
     executors: HashMap<Discriminant<StorageRequestType>, Executor<StorageRequestType>>,
+    cancellation_token: CancellationToken,
 }
 
 impl<StorageRequestType: Debug> Dispatch<StorageRequestType> {
     pub fn new(
         storage_request_receiver: Receiver<StorageRequestType>,
         executors: HashMap<Discriminant<StorageRequestType>, Executor<StorageRequestType>>,
+        cancellation_token: CancellationToken,
     ) -> Self {
         Self {
             storage_request_receiver,
             executors,
+            cancellation_token,
         }
     }
 
     pub async fn run(self) {
         loop {
+            if self.cancellation_token.is_cancelled() && self.storage_request_receiver.is_empty() {
+                info!("cancellation token is cancelled and storage request receiver is empty, storage dispatch is stopping");
+                break;
+            }
+
             let storage_request = match self.storage_request_receiver.recv().await {
                 Ok(storage_request) => storage_request,
                 Err(_) => {
